dd661385971a5e817e6602ae83d694e0032b3f29
haruhi-s <haruhi.s@protonmail.com>
2018-08-23 10:14:03 +0800

    now supports s-expressions

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,130 @@
 import * as vscode from 'vscode';
-import {Operation} from './operation';
+import { Operation } from './operation';
 
 var inMarkMode: boolean = false;
 export function activate(context: vscode.ExtensionContext): void {
+    //Start of my change
+    console.log('Congratulations, your extension "Emacs" is now sexp-capable!');
+    function nextSexpEnd(s) {
+        let atom_ = '[](){} \t\n\r';
+        let whites = ' \r\n\t';
+        let pos = 0;
+        function skip(that, toInclude) {
+            while (pos < s.length && toInclude === that.includes(s[pos])) {
+                pos++;
+            }
+        }
+        function skipSexp() {
+            skip(whites, true);
+            if (!atom_.includes(s[pos])) {
+                skip(atom_, false);
+            }
+            else if (s[pos] === '(') {
+                pos++;
+                skipParens(')');
+            }
+            else if (s[pos] === '[') {
+                pos++;
+                skipParens(']');
+            }
+            else if (s[pos] === '{') {
+                pos++;
+                skipParens('}');
+            }
+            return;
+        }
+        function skipParens(par) {
+            while (s[pos] !== par) {
+                if (pos >= s.length) {
+                    return;
+                }
+                let oldPos = pos;
+                skipSexp();
+                if (oldPos == pos) {
+                    break;
+                }
+            }
+            pos++;
+            return;
+        }
+        skipSexp();
+        return pos;
+    }
+    function selectionChanger(func) {
+        let editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+        let doc = editor.document;
+        func(editor, doc);
+    }
+    function getCurrentOffset(editor, doc) {
+        return doc.offsetAt(editor.selection.active);
+    }
+    function move(offset) {
+        selectionChanger((editor, doc) => {
+            let currentOffset = getCurrentOffset(editor, doc);
+            let anchor = editor.selection.anchor;
+            let newOffset = currentOffset + offset;
+            let newActive = doc.positionAt(newOffset);
+            editor.selection = new vscode.Selection(inMarkMode ? anchor : newActive, newActive);
+        });
+    }
+    function moveForwardSexp() {
+        selectionChanger((editor, doc) => {
+            let s = doc.getText().slice(getCurrentOffset(editor, doc));
+            let n = nextSexpEnd(s);
+            console.log(n);
+            move(n);
+        });
+    }
+    function slurpSexp() {
+        inMarkMode = true;
+        selectionChanger((editor, doc) => {
+            let anchorOffset = doc.offsetAt(editor.selection.anchor);
+            let s = doc.getText().slice(anchorOffset);
+            let n = nextSexpEnd(s);
+            anchorOffset += n;
+            let newAnchor = doc.positionAt(anchorOffset);
+            editor.selection = new vscode.Selection(newAnchor, editor.selection.active);
+        });
+    }
+    function moveBackwardSexp() {
+        selectionChanger((editor, doc) => {
+            let s = doc.getText().slice(0, getCurrentOffset(editor, doc)).split('').reverse().map((s) => {
+                if (s === '(') {
+                    return ')';
+                }
+                else if (s === ')') {
+                    return '(';
+                }
+                else if (s === '{') {
+                    return '}';
+                }
+                else if (s === '}') {
+                    return '{';
+                }
+                else if (s === '[') {
+                    return ']';
+                }
+                else if (s === ']') {
+                    return '[';
+                }
+                else {
+                    return s;
+                }
+            }).join('');
+            let n = nextSexpEnd(s);
+            console.log(n);
+            move(-n);
+        });
+    }
+    let subs = context.subscriptions;
+    subs.push(vscode.commands.registerCommand('emacs.moveForwardSexp', () => {}));
+    subs.push(vscode.commands.registerCommand('emacs.moveBackwardSexp', () => moveBackwardSexp()));
+    subs.push(vscode.commands.registerCommand('emacs.slurpSexp', () => slurpSexp()));
+    //end of my change
+
     let op = new Operation(),
         commandList: string[] = [
             "C-g",
@@ -28,11 +150,11 @@ export function activate(context: vscode.ExtensionContext): void {
 
     cursorMoves.forEach(element => {
         context.subscriptions.push(vscode.commands.registerCommand(
-            "emacs."+element, () => {
+            "emacs." + element, () => {
                 vscode.commands.executeCommand(
                     inMarkMode ?
-                    element+"Select" :
-                    element
+                        element + "Select" :
+                        element
                 );
             })
         )
@@ -40,10 +162,10 @@ export function activate(context: vscode.ExtensionContext): void {
 
     // 'type' is not an "emacs." command and should be registered separately
     context.subscriptions.push(vscode.commands.registerCommand("type", function (args) {
-		if (!vscode.window.activeTextEditor) {
-			return;
-		}
-		op.onType(args.text);        
+        if (!vscode.window.activeTextEditor) {
+            return;
+        }
+        op.onType(args.text);
     }));
 
     initMarkMode(context);
